15d6cbce19ff1f4318089d949057c74cdd08eb60
Ezequiel Bertti <ebertti@gmail.com>
2014-10-03 06:26:00 -0300

    fix project name on setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='requery',
+    name='django-admin-easy',
     version='0.1',
     url='http://github.com/ebertti/django-admin-easy/',
     author='Ezequiel Bertti',
