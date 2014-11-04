ce4b3637b92c454447a95dd75302f5cb9aa0bdbd
Ezequiel Bertti <ebertti@gmail.com>
2014-11-04 00:19:42 -0200

    add image field to test

--- test_app/models.py
+++ test_app/models.py
@@ -9,6 +9,7 @@ class Poll(models.Model):
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    image = models.ImageField(blank=True, null=True)
     poll = models.ForeignKey(Poll)
 
 
