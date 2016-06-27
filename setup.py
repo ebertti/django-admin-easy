83f34c9ca08491955d6562ba63238f6f618212b9
Ezequiel Bertti <ebertti@gmail.com>
2016-06-26 23:42:58 -0300

    Add python 3.4

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
     zip_safe=False,
