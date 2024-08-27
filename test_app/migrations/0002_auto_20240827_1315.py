db514d950d93b656672c54d98b73273c65280a93
Ezequiel Bertti <ezequiel.bertti@accenture.com>
2024-08-27 18:25:34 -0300

    fix object_id

--- test_app/migrations/0002_auto_20240827_1315.py
+++ test_app/migrations/0002_auto_20240827_1315.py
@@ -32,7 +32,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=50)),
-                ('object_id', models.PositiveBigIntegerField()),
+                ('object_id', models.BigIntegerField()),
                 ('content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype')),
             ],
         ),
