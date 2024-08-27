371e72dc27d2ace8f068c9430c76e69000fe9fe9
Ezequiel Bertti <ezequiel.bertti@accenture.com>
2024-08-27 19:37:16 -0300

    drop support for python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setup(
         'Natural Language :: Portuguese (Brazilian)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
